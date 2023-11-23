55ed1e6857e784ba993d1a6a29da66f94bf75b73
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2023-11-23 16:43:29 +0400

    delete imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Projects from "./pages/Projects";
 import Skills from "./pages/Skills";
-import Contact from "./pages/Contact";
-import {
-  createBrowserRouter,
-  Link,
-  Route,
-  RouterProvider,
-  Routes,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Landing from "./pages/Landing";
 
 const router = createBrowserRouter([
